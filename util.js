30441e375e3db3a938f8f19a394e86df016df8d3
Victor Diukov <32428849+ViktorDyukov@users.noreply.github.com>
2024-09-02 13:17:28 -0700

    8

--- util.js
+++ util.js
@@ -13,19 +13,9 @@ export function setupScrollIndicator() {
 }
 
 export function getScriptBaseUrl() {
-  // Получаем все теги <script> на странице
-  var scripts = document.getElementsByTagName("script");
-
-  // Последний скрипт в массиве — это тот, который выполняется
-  var currentScript = scripts[scripts.length - 1];
-
-  // Получаем полный URL текущего скрипта
-  var scriptURL = currentScript.src;
-
-  // Извлекаем путь к папке, где находится скрипт
-  var scriptPath = scriptURL.substring(0, scriptURL.lastIndexOf("/"));
-  console.log(scriptPath);
-
-  // Добавляем / в конце и возвращаем
-  return scriptPath + "/";
+  if (window.location.hostname.includes("localhost")) {
+    return "";
+  }
+  console.log("m2");
+  return "https://victorduco.github.io/portfolio24/";
 }
